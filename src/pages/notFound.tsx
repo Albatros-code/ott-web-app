037d22bcdd1d06e75adf3a445c16f7a0df865beb
Albatros-code <albert.kolakowski@gmail.com>
2021-08-08 09:43:56 +0200

    Not Found page styled

--- src/pages/notFound.tsx
+++ src/pages/notFound.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+// components
+import OttLogo from "../components/OttLogo";
 
 export default function notFound() {
   return (
-    <div>
-      <h1>page not found</h1>
-      <h1>404</h1>
+    <div className="not-found__wrapper">
+      <div className="not-found__comment">
+        <OttLogo size={5} color="secondary" />
+        <h1>404</h1>
+        <h2>Page not found.</h2>
+        <p>
+          Go to <Link to={"/"}>home page.</Link>
+        </p>
+      </div>
     </div>
   );
 }
